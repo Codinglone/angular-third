e103ea21cf3646c3dde3a6d37993eb5067a6d04a
codinglone <codinglone@gmail.com>
2023-06-22 17:03:31 +0400

    Added data sharing from child to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { BooksComponent } from './books/books.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-third';
-  message: string = "Hello from parent component"
+  message: string = "Hello from parent component";
+  messageFromChild: string = '';
+
+  @ViewChild(BooksComponent) fromChild!: BooksComponent
+
+  constructor(){
+    console.log(this.messageFromChild);
+    
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.messageFromChild);
+    
+    this.messageFromChild = this.fromChild.fromChildMessage;
+  }
 }
