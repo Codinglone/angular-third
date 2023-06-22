36bc3aeda4e67f83347d81429ed28ea8f3a01430
codinglone <codinglone@gmail.com>
2023-06-22 16:13:31 +0400

    Created books component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-books',
+    templateUrl: './books.component.html',
+    styleUrls: ['./books.component.css'],
+})
+
+
+export class BooksComponent {
+
+}
